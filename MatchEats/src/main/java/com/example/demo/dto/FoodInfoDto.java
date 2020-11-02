13bd5b09d90526e1793002898e34bd90c6b794d4
Oka Hiroki <greeeen010636@gmail.com>
2020-11-02 13:42:39 +0900

    FoodInfoDtoを作成

---
+++ MatchEats/src/main/java/com/example/demo/dto/FoodInfoDto.java
@@ -0,0 +1,68 @@
+package com.example.demo.dto;
+
+import java.util.Date;
+
+public class FoodInfoDto {
+
+	private String requestId;
+	private int userId;
+	private String foodname;
+	private String requestOutline;
+	private Date registDate;
+	private String genreId;
+	private String eatFlag;
+	private String requestPicture;
+
+	public String getRequestId() {
+		return requestId;
+	}
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getFoodname() {
+		return foodname;
+	}
+	public void setFoodname(String foodname) {
+		this.foodname = foodname;
+	}
+	public String getRequestOutline() {
+		return requestOutline;
+	}
+	public void setRequestOutline(String requestOutline) {
+		this.requestOutline = requestOutline;
+	}
+	public Date getRegistDate() {
+		return registDate;
+	}
+	public void setRegistDate(Date registDate) {
+		this.registDate = registDate;
+	}
+	public String getGenreId() {
+		return genreId;
+	}
+	public void setGenreId(String genreId) {
+		this.genreId = genreId;
+	}
+	public String getEatFlag() {
+		return eatFlag;
+	}
+	public void setEatFlag(String eatFlag) {
+		this.eatFlag = eatFlag;
+	}
+	public String getRequestPicture() {
+		return requestPicture;
+	}
+	public void setRequestPicture(String requestPicture) {
+		this.requestPicture = requestPicture;
+	}
+
+
+
+
+}

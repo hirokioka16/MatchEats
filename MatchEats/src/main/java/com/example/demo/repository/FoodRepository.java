fd4fff51c263483be818f16de4e64afcd8ed19f9
FuzimoriSingo <1801177@s.asojuku.ac.jp>
2020-11-06 12:07:46 +0900

    食べたい物リスト修正入力、確認

--- MatchEats/src/main/java/com/example/demo/repository/FoodRepository.java
+++ MatchEats/src/main/java/com/example/demo/repository/FoodRepository.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 
 import com.example.demo.entity.FoodTblEntity;
-import com.example.demo.entity.GenreTblEntity;
-import com.example.demo.entity.UserTblEntity;
+
 
 
 
@@ -24,4 +24,9 @@ public interface FoodRepository extends JpaRepository<FoodTblEntity,Integer>{
 	//自分の投稿した食べたい物リストを取得する
 	@Query("SELECT f FROM FoodTblEntity f left join f.userTbl u WHERE u.userId = :userId")
 	public List<FoodTblEntity> getMyFoodList(@Param("userId") int userId);
+	
+	//食べたい物リスト修正
+	@Modifying
+	@Query("SELECT f FROM FoodTblEntity f left join f.userTbl u WHERE u.userId = :userId")
+	public void update();
 }

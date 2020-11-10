bf9234c4307c21a7c321d028302b83aaee356438
Oka Hiroki <greeeen010636@gmail.com>
2020-11-10 15:18:08 +0900

    入力内容確認まで完成してるはず

--- MatchEats/src/main/java/com/example/demo/controller/CookingOfferController.java
+++ MatchEats/src/main/java/com/example/demo/controller/CookingOfferController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -17,13 +19,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.dto.CookingInfoDto;
 import com.example.demo.dto.FoodInfoDto;
-import com.example.demo.dto.GenreInfoDto;
+import com.example.demo.dto.LoginInfoDto;
 import com.example.demo.form.CookingForm;
+import com.example.demo.service.CookingOfferService;
 import com.example.demo.service.FoodService;
 
 @Controller
-@RequestMapping(value= {"/offerinput"})
+@RequestMapping(value= {"/inputoffer"})
 public class CookingOfferController {
 
 	@Autowired
@@ -35,7 +39,11 @@ public class CookingOfferController {
 	public String input(@RequestParam("requestId") String requestId, @ModelAttribute("CookingForm")CookingForm form,Model model) {
 
 		//オファーを送る食べたいものの情報を再取得
+		int r_Id = Integer.parseInt(requestId);
 		FoodInfoDto dto = foodService.getUdFoodList(Integer.parseInt(requestId));
+
+		session.setAttribute("requestId", r_Id);
+
 
 		model.addAttribute("dto",dto);
 
@@ -45,33 +53,91 @@ public class CookingOfferController {
 	@RequestMapping(value= {"/confirm"}, method=RequestMethod.POST)
 	public String confirm (@Validated @ModelAttribute("CookingForm")CookingForm form,BindingResult result,Model model) throws IllegalStateException, IOException {
 
+		//画面の遷移先を保持する文字型変数
+		String url = null;
+
 		//入力エラーをチェック
 				if(result.hasErrors()) {
 					List<String> errorList = new ArrayList<String>();
 					for(ObjectError error : result.getAllErrors()) {
 						errorList.add(error.getDefaultMessage());
 					}
-					List<GenreInfoDto> list = foodService.getGenre();
 					model.addAttribute("validationError", errorList);
-					model.addAttribute("list",list);
-
+					//エラーあり入力画面に戻す
+					url = "input_offer";
 				}else {
+					//入力内容に問題がない時の処理
+					CookingInfoDto dto = getCreateDto(form);
 
+					//sessionよりLoginInfoを取得
+					LoginInfoDto loginInfo = (LoginInfoDto)session.getAttribute("loginInfo");
+					//userIdを取り出しDtoに格納
+					//dto.setUserId(loginInfo.getUserId());
+
+					//テスト用
+					dto.setUserId("1");
+
+					session.setAttribute("CookingInfoDto", dto);
+
+					url="confirm_offer";
 				}
+		return url;
+
+	}
+
+	@RequestMapping(value= {"/insert"}, method=RequestMethod.POST)
+	public String insert() throws java.text.ParseException {
+
+		CookingInfoDto dto = (CookingInfoDto)session.getAttribute("CookingInfoDto");
+		dto.setOfferDate(getNowDate());
+		int requestId = (int)session.getAttribute("requestId");
+		dto.setRequestId(String.valueOf(requestId));
+
+		CookingOfferService.insert(dto);
+
+
+		session.removeAttribute("CookingInfoDto");
+		return "redirect:/inputoffer/complete";
+	}
+
+	@RequestMapping(value= {"/complete"}, method=RequestMethod.GET)
+	public String complete() {
+
+		//オファーが来たことをメールで通知したい人生だった
+		LoginInfoDto loginInfo = (LoginInfoDto)session.getAttribute("loginInfo");
+
+		String email = loginInfo.getEmail();
+		session.removeAttribute("requestId");
+
+		return "complete_offer_input";
+
+	}
 
 
 
+	//formで入力した値をdtoに挿入するメソッド
+	public CookingInfoDto getCreateDto(CookingForm form) {
+
+		CookingInfoDto dto = new CookingInfoDto();
+
+		dto.setPrice(form.getPrice() );
+		dto.setOfferComment(form.getOfferComment());
+
+		return dto;
+
+	}
 
 
+	//現在時刻を登録する
+	public Date getNowDate() throws java.text.ParseException {
+		Date date = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		String strDate = dateFormat.format(date);
+		//String型の日付をDate型に変更している
+		SimpleDateFormat d1 = new SimpleDateFormat("yyyy-MM-dd");
+		Date now = d1.parse(strDate);
 
-
-
-
-
-
-
-		return null;
-
+		return now;
 	}
 
 
--- MatchEats/src/main/java/com/example/demo/service/CookingOfferService.java
+++ MatchEats/src/main/java/com/example/demo/service/CookingOfferService.java
@@ -1,5 +1,15 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.CookingInfoDto;
+
+@Service
 public class CookingOfferService {
+
+	public void insert(CookingInfoDto dto) {
+
+		CookOfferTblEntity offerEntity = insert
+	}
 
 }

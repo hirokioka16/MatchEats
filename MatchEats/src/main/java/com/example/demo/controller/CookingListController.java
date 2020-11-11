d64a6dcfdd1e6f530000a540080d9adc8c4b8a1b
kourogi <kourgi33@gmail.com>
2020-11-11 15:57:46 +0900

    調理リスト一覧仮完成

--- MatchEats/src/main/java/com/example/demo/controller/CookingListController.java
+++ MatchEats/src/main/java/com/example/demo/controller/CookingListController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.dto.FoodInfoDto;
 import com.example.demo.service.CookingOfferService;
 
 @Controller
@@ -16,11 +19,13 @@ private CookingOfferService cookService;
 @GetMapping(value="/cooking/list")
 public String displayList(Model model) {
 
-	cookService.getList();
+	List<FoodInfoDto> CookingList=cookService.getList();
 
 	//List<CookingInfoDto>cookinglist = FoodService.searchAll();
 	//model.addAttribute("cokkinglist","cookinglist");
-	return "cooking/list";
+
+	model.addAttribute("CookingList", CookingList);
+	return "my_cook_list";
 
 }
 
--- MatchEats/src/main/java/com/example/demo/service/CookingOfferService.java
+++ MatchEats/src/main/java/com/example/demo/service/CookingOfferService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.CookingInfoDto;
+import com.example.demo.dto.FoodInfoDto;
 import com.example.demo.entity.CookOfferTblEntity;
+import com.example.demo.entity.FoodTblEntity;
 import com.example.demo.repository.CookingRepository;
 
 @Service
@@ -16,12 +18,31 @@ public class CookingOfferService {
 	@Autowired
 	private CookingRepository cookingRepository;
 
-	public  List<CookingInfoDto> getList(){
+	public  List<FoodInfoDto> getList(){
 		List<CookingInfoDto> list = new ArrayList<CookingInfoDto>();
 		 List<CookOfferTblEntity> tblList = cookingRepository.getList();
 
 
-		return null;
+		 List<FoodTblEntity> resultList =  new ArrayList<FoodTblEntity>();
+		 List<FoodInfoDto> resultListDto = new ArrayList<FoodInfoDto>();
+		 for(int i=0;i<tblList.size();i++) {
+			resultList.add(tblList.get(i).getFoodTbl());
+
+		 }
+
+		 for(FoodTblEntity entity:resultList) {
+
+				FoodInfoDto dto = new FoodInfoDto();
+				dto.setFoodName(entity.getFoodName());
+				dto.setRegistDate(entity.getRegistDate());
+				dto.setPictureName(entity.getRequestPicture());
+
+				resultListDto.add(dto);
+
+
+			}
+
+		return resultListDto;
 	}
 
 }

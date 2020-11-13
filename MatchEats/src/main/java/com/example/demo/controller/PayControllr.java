308127c052a2f0c76cdb019661f4573962f5cba3
FuzimoriSingo <1801177@s.asojuku.ac.jp>
2020-11-13 10:28:36 +0900

    決済処理完了(決済する料理をDBから持ってくる処理は書いてない)

---
+++ MatchEats/src/main/java/com/example/demo/controller/PayControllr.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.form.StripeForm;
+import com.example.demo.service.PayService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+
+
+
+@Controller
+public class PayControllr {
+	
+	@Autowired
+	private PayService payService;
+	
+	   @RequestMapping(value = {"/pay"},method=RequestMethod.GET)
+	   public String checkout(Model model) {
+
+		
+		//料理名、料理の説明、金額をDBからとってくる
+		   
+		
+		model.addAttribute("amount", 100);//in cents
+		model.addAttribute("foodName", "テスト商品名");
+		model.addAttribute("content", "テスト料理説明");
+        model.addAttribute("stripePublicKey", "pk_test_51HZSIrJjnozX4HQuaoN1pAVnhJ3pwgaEytjzaPwWIbgK5edPnoTMkELL69n40RGw6mdOW6RIqFjIGfsQ6WVos4XB00TEoEImT5");
+        model.addAttribute("currency", "jpy");
+	       return "stripe_checkout";
+	   }
+	   
+	   @RequestMapping(value = {"/charge"},method=RequestMethod.POST)
+	    public String charge(StripeForm stripeForm, Model model)throws StripeException {
+	    	stripeForm.setDescription("Example charge");
+	        Charge charge = payService.charge(stripeForm);
+	       
+	        return "pay_complete";
+	    }
+	   
+	   @ExceptionHandler(StripeException.class)
+	    public String handleError(Model model, StripeException ex) {
+	        model.addAttribute("error", ex.getMessage());
+	        return "pay_complete";
+	    }
+}
---
+++ MatchEats/src/main/java/com/example/demo/service/PayService.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.form.StripeForm;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+
+@Service
+public class PayService {
+	
+	public Charge charge(StripeForm stripeForm)throws StripeException{
+	Stripe.apiKey = "sk_test_51HZSIrJjnozX4HQuaJEH2KwtoFV15jNAuHzoMFjAD33VhGCo4bBRHML5c6u29yqwVxkhYEU1SI56nFmoVj5zv7wk00EEGDaZ1p";
+
+    Map<String, Object> chargeMap = new HashMap<String, Object>();
+    chargeMap.put("amount", stripeForm.getAmount());
+    chargeMap.put("description", stripeForm.getDescription());
+    chargeMap.put("currency", "jpy");
+    chargeMap.put("source", stripeForm.getStripeToken());
+    
+    //reaction_statusを"3"(決済済み)に設定するRepositoryを書く
+
+    return Charge.create(chargeMap);
+	}
+}

098c7636e9e17b7ad1af2e89b8f75fa94a509ec4
kourogi <kourgi33@gmail.com>
2020-12-01 14:01:06 +0900

    12/1

--- MatchEats/src/main/java/com/example/demo/controller/DetailMyFoodListController.java
+++ MatchEats/src/main/java/com/example/demo/controller/DetailMyFoodListController.java
@@ -15,22 +15,24 @@ import com.example.demo.dto.LoginInfoDto;
 import com.example.demo.service.FoodService;
 @Controller
 public class DetailMyFoodListController {
-	
+
 	@Autowired
-	FoodService foodService; 
+	FoodService foodService;
 	@Autowired
 	HttpSession session;
-	
+
 	@RequestMapping(value= {"/foodlist"}, method=RequestMethod.GET)
 	public String getUserList(Model model) {
-		
+
 		session.removeAttribute("foodInfDto");
-		
+
 		LoginInfoDto loginInfo  = (LoginInfoDto)session.getAttribute("loginInfo");
-	
+
 		List<FoodInfoDto> list = foodService.getMyFoodList(loginInfo.getUserId());
 		model.addAttribute("list", list);
 		return "my_food_userlist";
 	}
 
 }
+
+

9e4ac521bc220b267ebacab5dce9a753c886a653
FuzimoriSingo <1801177@s.asojuku.ac.jp>
2020-12-04 19:39:50 +0900

    振り込み申請一覧できた

--- MatchEats/src/main/java/com/example/demo/repository/TransferRepository.java
+++ MatchEats/src/main/java/com/example/demo/repository/TransferRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import java.util.Date;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +14,7 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entity.TransferId;
 import com.example.demo.entity.TransferTblEntity;
 
-
+@Transactional
 @Repository
 public interface TransferRepository extends JpaRepository<TransferTblEntity,TransferId>{
 
@@ -21,8 +23,11 @@ public interface TransferRepository extends JpaRepository<TransferTblEntity,Tran
 	public List<TransferTblEntity> getAll();
 
 	@Modifying
-	@Query("UPDATE TransferTblEntity t SET t.acceptDate = :now WHERE t.transferId = :transferId")
-	public void insertApproval(@Param("now") Date now,@Param("transferId") TransferId transferId);
+	@Query("UPDATE TransferTblEntity t SET"
+			+ " t.acceptDate = :now, "
+			+ " t.acceptFlag = true "
+			+ "WHERE t.transferId.transferId = :transferId")
+	public void insertApproval(@Param("now") Date now,@Param("transferId") int transferId);
 
 	//振込申請一覧取得
 	//@Query("SELECT t FROM TransferTblEntity t WHERE t");
@@ -37,6 +42,10 @@ public interface TransferRepository extends JpaRepository<TransferTblEntity,Tran
 //			@Param("transferDate") Date date,
 //			@Param("userId") int userId
 //			);
+	
+	//振り込み申請詳細
+	@Query("SELECT t FROM TransferTblEntity t WHERE t.transferId.transferId = :transferid")
+	public TransferTblEntity getTransfer(@Param("transferid") int transferid);
 }
 
 

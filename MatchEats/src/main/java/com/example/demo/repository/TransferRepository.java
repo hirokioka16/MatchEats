934430378785821a137b9eae12668caf0b8ed937
Oka Hiroki <greeeen010636@gmail.com>
2020-12-04 09:37:33 +0900

    Auto stash before merge of "main" and "origin/main"

--- MatchEats/src/main/java/com/example/demo/repository/TransferRepository.java
+++ MatchEats/src/main/java/com/example/demo/repository/TransferRepository.java
@@ -1,6 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.TransferId;
@@ -9,6 +15,14 @@ import com.example.demo.entity.TransferTblEntity;
 
 @Repository
 public interface TransferRepository extends JpaRepository<TransferTblEntity,TransferId>{
+
+	@Modifying
+	@Query("SELECT t FROM TransferTblEntity t WHERE t.acceptFlag = false")
+	public List<TransferTblEntity> getAll();
+
+	@Modifying
+	@Query("UPDATE TransferTblEntity t SET t.acceptDate = :now WHERE t.transferId = :transferId")
+	public void insertApproval(@Param("now") Date now,@Param("transferId") TransferId transferId);
 
 	//振込申請一覧取得
 	//@Query("SELECT t FROM TransferTblEntity t WHERE t");
